5c02a5fb2e7876315ebd264a55285b60d3092220
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-12-20 14:26:49 -0800

    Update and rename LegbaEvents.php to LegbaEvent.php

--- LegbaEvent.php
+++ LegbaEvent.php
@@ -1,6 +1,6 @@
 <?php
 
-class LegbaEvents{
+class LegbaEvent{
     /*
     Events
     
@@ -10,15 +10,13 @@ class LegbaEvents{
     
   */
   
-  private $Events = array(); //This will hold all the events and their code
-  
   public function event($eventDescription, $defaultFunction){
     //Handle the execution of an event and any code which is hooked onto it.
     //Add information to the debug variable pertaining to the runtime and memory usage of this event.
     
   }
   
-  public function event()->hook($eventDescription, $Code, $position = 'bottom'){
+  public function hook($eventDescription, $Code, $position = 'bottom'){
     //Hook an code block onto an event. It will be executed when that event is called.
     //The optional position parameter specifies where in the event's queue this block will execute. Valid options are top or bottom.
     
