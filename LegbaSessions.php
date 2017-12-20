a6f380bcdd85838b4b35b9e3279071e1d466c7a3
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-12-20 11:45:18 -0800

    Create LegbaSessions.php

---
+++ LegbaSessions.php
@@ -0,0 +1,21 @@
+<?php
+
+class LegbaSessions{
+  
+  
+  private function init(){
+    //Checks if there is a session. Loads it from cache, or creates a new one.
+    
+  }
+  
+  public function save(){
+    //Saves the current session to the database if it is different than the last time it was saved. Page is not interrupted.
+    
+  }
+  
+  public function destroy(){
+    //Destroys the current session and redirects to the homepage
+    
+  }
+  
+}
