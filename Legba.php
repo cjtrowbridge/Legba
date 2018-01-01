700ee236548c997a3e6b8c7bdcf329749fddd150
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-12-31 17:52:47 -0800

    Update Legba.php

--- Legba.php
+++ Legba.php
@@ -58,7 +58,7 @@ class Legba{
   function __construct(&$arrEnvironment = null){
     //Check for config file and load it or prompt to create one
     $this->objConfig = new LegbaConfig($this, $arrConfig, $arrEnvironment);
-    $this->arrConfig = $this->objConfig->load($this, $arrConfig, $arrEnvironment);
+    $this->objConfig->load($this, $arrConfig, $arrEnvironment);
     
     var_dump($this->config());
     
